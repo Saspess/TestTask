ad9f4435a881d7154089155387ac29d845ce8946
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-10-04 23:12:24 +0300

    Add services

---
+++ TestTask/Services/Implementation/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using TestTask.Models;
+using TestTask.Repositories.Interfaces;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementation
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Order> GetOrder() => 
+            await _orderRepository.GetOrder();
+
+        public async Task<List<Order>> GetOrders() => 
+            await _orderRepository.GetOrders();
+    }
+}
